3d3d141baea8486cffc8139e79bc3b960b819f4f
dsiguero <26867647+dsiguero@users.noreply.github.com>
2021-08-30 11:51:44 +0200

    [code] Add health check route and pass x-forwarded-ip as req.ip to display the real client address

--- factorial/app.js
+++ factorial/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const factorial = require('./factorial');
 
@@ -6,12 +7,21 @@ const app = express();
 
 const PORT = 8080;
 
+app.set('trust proxy', true);
+
+app.use(morgan('combined'));
+
 app.get('/', function (req, res) {
     const { number } = req.query;
 
     res.write(`The number is: ${number}.\n`);
     res.write(`Its factorial value is: ${factorial(number)}.`);
     res.end();
+});
+
+
+app.get('/health', function (req, res) {
+    res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
